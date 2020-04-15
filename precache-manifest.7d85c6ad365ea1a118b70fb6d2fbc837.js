019211c90fdc578efbb392586390ab4662c2e9e0
Keval navadiya <31183263+kk-007@users.noreply.github.com>
2020-04-15 20:03:09 +0530

    Updates

---
+++ precache-manifest.7d85c6ad365ea1a118b70fb6d2fbc837.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b295c0dae5de6bcc0e59cb3745c515a",
+    "url": "/tic-tac-toe-minmax-react/index.html"
+  },
+  {
+    "revision": "6db053ca06fb2e135ca1",
+    "url": "/tic-tac-toe-minmax-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d02f91a5be919f4f6a93",
+    "url": "/tic-tac-toe-minmax-react/static/js/2.f7c22329.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tic-tac-toe-minmax-react/static/js/2.f7c22329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6db053ca06fb2e135ca1",
+    "url": "/tic-tac-toe-minmax-react/static/js/main.7ae3ef3f.chunk.js"
+  },
+  {
+    "revision": "636b278bcac481c6964c",
+    "url": "/tic-tac-toe-minmax-react/static/js/runtime-main.ec0a4254.js"
+  }
+]);
